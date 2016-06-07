b45d2271f6df19cecd1d8a8955fdde5c7edca9ef
ebouilhol <emmanuel.bouilhol@gmail.com>
2016-06-07 09:37:06 +0200

    improve sql queries

--- php/batches/batch_is_used.php
+++ php/batches/batch_is_used.php
@@ -15,55 +15,7 @@
 	elseif($infos[0] =="?"){
 		$status="success";
 	}
-	/*else {
-/*		$query = "
-		SELECT 
-		count(*) 
-		FROM
-		freshweight, freshweight_sample, sample, sample_aliquot, aliquot 
-		WHERE 
-		freshweight.fw_name like '%".$infos[0]."%' AND 
-		freshweight_sample.fw_spl_freshweight_FK = freshweight.fw_id AND
-		sample.spl_id = freshweight_sample.fw_spl_sample_FK AND
-		sample_aliquot.spl_alq_sample_FK =sample.spl_id AND
-		sample.spl_number = ".$infos[1]." AND 
-		aliquot.alq_id = sample_aliquot.spl_alq_aliquot_FK AND
-		sample_aliquot.spl_alq_state like '%free%' AND
-		aliquot.alq_number = ".$infos[2].";";
-
-
-		try 
-		{
-			$stmt = $conn->prepare("SELECT 
-			count(*) 
-			FROM
-			freshweight, freshweight_sample, sample, sample_aliquot, aliquot 
-			WHERE 
-			freshweight.fw_name like '%:infos0%' AND 
-			freshweight_sample.fw_spl_freshweight_FK = freshweight.fw_id AND
-			sample.spl_id = freshweight_sample.fw_spl_sample_FK AND
-			sample_aliquot.spl_alq_sample_FK =sample.spl_id AND
-			sample.spl_number = :infos1 AND 
-			aliquot.alq_id = sample_aliquot.spl_alq_aliquot_FK AND
-			sample_aliquot.spl_alq_state like '%free%' AND
-			aliquot.alq_number = :infos2");
-
-			$stmt->bindValue(':infos0', $infos[0], PDO::PARAM_STR);
-			$stmt->bindValue(':infos1', $infos[1], PDO::PARAM_INT);
-			$stmt->bindValue('infos2', $infos[2], PDO::PARAM_INT);
-			$stmt->execute();
-			$result = $stmt->fetchColumn();
-			if ($result > 0) {
-				$status="success";
-			}
-		}
-		catch ( Exception $e ) {
-			error_log("failure est survenue lors de $query".$e->getMessage());
-			$status = "error";
-		}
-	}*/
-
-	else {
+/*	else {
 		$query = "
 		SELECT 
 		count(*) 
@@ -90,7 +42,42 @@
 			error_log("failure est survenue lors de $query".$e->getMessage());
 			$status = "error";
 		}
+	}*/
+
+		try 
+		{
+			$stmt = $conn->prepare("SELECT 
+			count(*) 
+			FROM
+			freshweight, freshweight_sample, sample, sample_aliquot, aliquot 
+			WHERE 
+			freshweight.fw_name like '%:infos0%' AND 
+			freshweight_sample.fw_spl_freshweight_FK = freshweight.fw_id AND
+			sample.spl_id = freshweight_sample.fw_spl_sample_FK AND
+			sample_aliquot.spl_alq_sample_FK =sample.spl_id AND
+			sample.spl_number = :infos1 AND 
+			aliquot.alq_id = sample_aliquot.spl_alq_aliquot_FK AND
+			sample_aliquot.spl_alq_state like '%free%' AND
+			aliquot.alq_number = :infos2");
+
+			$stmt->bindValue(':infos0', $infos[0], PDO::PARAM_STR);
+			$stmt->bindValue(':infos1', $infos[1], PDO::PARAM_INT);
+			$stmt->bindValue(':infos2', $infos[2], PDO::PARAM_INT);
+			$stmt->execute();
+
+			$sth->debugDumpParams();
+
+			$result = $stmt->fetchColumn();
+			if ($result > 0) {
+				$status="success";
+			}
+		}
+		catch ( Exception $e ) {
+			error_log("failure est survenue lors de $query".$e->getMessage());
+			$status = "error";
+		}
 	}
+
 
 	$response_array['status']=$status;
 	echo json_encode($response_array);
